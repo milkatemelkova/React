48e6cba12ba091dd8ddeb7d11bdf44aac8bf633d
milkatemelkova <milkatemelkova@abv.bg>
2020-03-06 14:31:32 +0200

    props and destructing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import { argumentPlaceholder } from '@babel/types';
+//import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+function People () {
+const friends = [
+    {name: "John Doe", job: "developer", age: "22", company: "apple"},
+    {name: "Bob Doe", job: "designer", age: "24", company: "facebook"},
+    {name: "Susy Doe", job: "artist", age: "25", company: "google"}
+];
+
+    return (
+        <section>
+           <Person  person={friends[0]} />
+           <Person  person={friends[1]} />
+           <Person  person={friends[2]} />
+        </section>
+    );
+}
+const Person = props => {
+    const { name, job, age, company } = props.person;
+return (
+        <article>
+            <h1>{name}</h1>
+            <p>{job}</p>
+            <p>{age}</p>
+            <hr />
+        </article>
+    );
+};
+
+ReactDom.render(<People />,document.getElementById('root'));
